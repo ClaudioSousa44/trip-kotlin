2bb561361fafbf15c0cab920313ea2447a7c9df7
CLAUDIO HENRIQUE FERREIRA DE SOUSA <22282470@dsn127m1242314.cfp127aluno.local>
2023-03-21 16:54:51 -0300

    continuando a home screen

--- app/src/main/java/br/senai/sp/jandira/login_screen/Home.kt
+++ app/src/main/java/br/senai/sp/jandira/login_screen/Home.kt
@@ -5,14 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.login_screen.ui.theme.LoginscreenTheme
 
 class Home : ComponentActivity() {
@@ -41,14 +48,46 @@ fun HomeScreen() {
 
                 ) {
                 Image(painter = painterResource(id = R.drawable.paris), contentDescription = "" )
-                Column() {
-                    Surface() {
-                        Image(painter = painterResource(id = R.drawable.susanna_profile), contentDescription = "")
+                Column(modifier = Modifier.padding(end = 19.dp, top = 13.dp, start = 22.dp)) {
+                    Row(modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End) {
+
+                        Surface(modifier = Modifier
+                            .height(61.dp)
+                            .width(61.dp),
+                            shape = CircleShape) {
+                            Image(painter = painterResource(id = R.drawable.susanna_profile), contentDescription = "")
+
+                        }
                     }
-                    
+                    Row(modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End) {
+                        Text(text = "Susanna Hoffs",
+                            color = Color.White,
+                            fontSize = 12.sp
+                        )
+                    }
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_location_on_24),
+                            contentDescription = "",
+                            tint = Color.White,
+                            modifier = Modifier
+                                .height(14.dp)
+                                .width(14.dp)
+
+                        )
+                        Text(text = stringResource(id = R.string.you_are_in_paris),
+                            color = Color.White,
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight(400)
+                        )
+                    }
+                    Text(text = stringResource(id = R.string.my_trip),
+                        color = Color.White,
+                        fontWeight = FontWeight(700),
+                        fontSize = 24.sp)
                 }
-
-
             }
         }
 
