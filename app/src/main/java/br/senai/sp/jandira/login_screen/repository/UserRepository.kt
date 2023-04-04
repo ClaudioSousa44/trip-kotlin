6393604b9d50d0c5c8c877f6aca423a5b0b91a34
CLAUDIO HENRIQUE FERREIRA DE SOUSA <22282470@dsn127m1242314.cfp127aluno.local>
2023-04-04 16:51:14 -0300

    fazendo o room

---
+++ app/src/main/java/br/senai/sp/jandira/login_screen/repository/UserRepository.kt
@@ -0,0 +1,14 @@
+package br.senai.sp.jandira.login_screen.repository
+
+import android.content.Context
+import br.senai.sp.jandira.login_screen.DAO.TripDb
+import br.senai.sp.jandira.login_screen.model.User
+
+class UserRepository(context: Context) {
+
+    private   val db = TripDb.getDataBase(context)
+
+    fun save(user: User): Long{
+        return  db.userDao().save(user)
+    }
+}
